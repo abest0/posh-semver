dde2ea65cdf352aad4ad8bd66e3525255fa8b414
bahrens <bahrens@sage-quest.com>
2013-04-05 11:58:53 -0400

    renamed semver.psm1 to semver.ps1

--- NuGetPackageBuilder.ps1
+++ NuGetPackageBuilder.ps1
@@ -1,1 +1,1 @@
-mkdir .\bin\nuget\tools -forcegci -path .\source | cp -destination .\bin\nuget\toolsgci -path .\nuget | cp -destination .\bin\nuget\.\nuget pack .\bin\nuget\posh-semver.nuspec -outputdirectory .\bin\nuget
+rmdir .\bin -force -recursemkdir .\bin\nuget\toolsgci -path .\source | cp -destination .\bin\nuget\toolsgci -path .\nuget | cp -destination .\bin\nuget\.\nuget pack .\bin\nuget\posh-semver.nuspec -outputdirectory .\bin\nuget
